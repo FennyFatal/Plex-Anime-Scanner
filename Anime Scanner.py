e3c45d9c974303b44dab84c316a5aff8acd84600
Florin Stancu <stancu.florin23@gmail.com>
2018-01-02 12:13:14 +0200

    added new regex

--- Anime Scanner.py
+++ Anime Scanner.py
@@ -4,39 +4,68 @@ import re, os, inspect, logging
 LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), "..", "..", "Logs"))
 logging.basicConfig(filename=os.path.join(LOG_PATH, 'Anime Scanner.log'), level=logging.DEBUG)
 
-regexSeason = '(?P<show>.*?)[\._\- ]+[sS](?P<season>[0-9]{2})'             # S08
-regexEpisode = '(?P<show>.*?)[\._\- EP]+(?P<episode>[0-9]+)[\._\- ]+'      # 160 or EP160
+regexSeason = '(?P<show>.*?)[S](?P<season>[0-9]{2})'  # S08
+regexEpisode = '(?P<show>.*?)(E|EP|Episodul|Special)?[\._\- ]*(?P<episode>[0-9]+)[\._\- ]+'  # 160 or EP160 or Special.02
+regexMovie = '(?P<show>.*?)(Movie|M)[\._\- ]*(?P<movie>[0-9]+)[\._\- ]+'  # Movie.03 or Movie03 or M.03 or M03
+
+
+def getAnime(pathAnime):
+    (animeName, animeYear) = VideoFiles.CleanName(pathAnime)
+    logging.debug("Anime Name: %s" % animeName)
+    logging.debug("Anime Year: %s" % animeYear)
+    return (animeName, animeYear)
+
+
+def getSeason(pathSeason):
+    seasonNumberMatch = re.search(regexSeason, pathSeason, re.IGNORECASE)
+    if seasonNumberMatch:
+        seasonNumber = seasonNumberMatch.group('season')
+    else:
+        seasonNumber = 1
+
+    logging.debug("Season Number: %s" % seasonNumber)
+    return seasonNumber
+
+
+def findEpisodes(animeName, animeYear, seasonNumber, files, mediaList):
+    for filePath in files:
+        file = os.path.basename(filePath)
+        episodeNumberMatch = re.search(regexEpisode, file, re.IGNORECASE)
+        movieNumberMatch = re.search(regexMovie, file, re.IGNORECASE)
+        if movieNumberMatch:
+            movieNumber = movieNumberMatch.group('movie')
+            logging.debug("File: %s" % filePath)
+            logging.debug("Movie Number: %s" % movieNumber)
+            anime = Media.Movie(animeName + ' Movie ' + movieNumber, None)
+            anime.parts.append(filePath)
+            mediaList.append(anime)
+        elif episodeNumberMatch:
+            episodeNumber = episodeNumberMatch.group('episode')
+            logging.debug("File: %s" % filePath)
+            logging.debug("Episode Number: %s" % episodeNumber)
+            anime = Media.Episode(animeName, seasonNumber, episodeNumber, None, animeYear)
+            anime.parts.append(filePath)
+            mediaList.append(anime)
+
 
 def Scan(path, files, mediaList, subdirs):
+    VideoFiles.Scan(path, files, mediaList, subdirs)
+    paths = Utils.SplitPath(path)
 
-	VideoFiles.Scan(path, files, mediaList, subdirs)
-	paths = Utils.SplitPath(path)
+    logging.debug("---------------------------------------------------")
+    logging.debug("Paths: %s" % paths)
 
-	logging.debug("---------------------------------------------------") 
-	logging.debug("Paths: %s" % paths)
-	
-	if len(paths) == 2:
-		pathAnime = paths[0]
-		pathSeason = paths[1]
-		(animeName, animeYear) = VideoFiles.CleanName(pathAnime)
-		logging.debug("Anime Name: %s" % animeName) 
-		logging.debug("Anime Year: %s" % animeYear)
-		
-		seasonNumberMatch = re.search(regexSeason, pathSeason, re.IGNORECASE)
-		if seasonNumberMatch:
-			seasonNumber = seasonNumberMatch.group('season')
-			logging.debug("Season Number: %s" % seasonNumber)
-		else:
-			seasonNumber = 1
-			
-		for file in files:
-			episodeNumberMatch = re.search(regexEpisode, file, re.IGNORECASE)
-			if episodeNumberMatch:
-				episodeNumber = episodeNumberMatch.group('episode')
-				logging.debug("File: %s" % file)
-				logging.debug("Episode Number: %s" % episodeNumber)
-				anime = Media.Episode(animeName, seasonNumber, episodeNumber, None, animeYear)
-				anime.parts.append(file)
-				mediaList.append(anime)
-				
-	Stack.Scan(path, files, mediaList, subdirs)
+    if len(paths) == 1 and len(paths[0]) > 0:
+        pathAnime = paths[0]
+        (animeName, animeYear) = getAnime(pathAnime)
+        findEpisodes(animeName, animeYear, 1, files, mediaList)
+
+    elif len(paths) == 2:
+        pathAnime = paths[0]
+        (animeName, animeYear) = getAnime(pathAnime)
+
+        pathSeason = paths[1]
+        seasonNumber = getSeason(pathSeason)
+        findEpisodes(animeName, animeYear, seasonNumber, files, mediaList)
+
+    Stack.Scan(path, files, mediaList, subdirs)
